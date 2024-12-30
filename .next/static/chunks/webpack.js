fc5ac7af9d0d738fa810cc8ddcec0733a3f8cde3
Terrance <terrancehah@gmail.com>
2024-12-31 02:34:28 +0800

    fix stageprogress parameter and ispaid reference

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c83771fe9b8260c8"; }
+/******/ 		__webpack_require__.h = function() { return "6706983adbcb3c5f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
