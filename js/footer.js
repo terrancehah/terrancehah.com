9cf8a42ca04cc0f6391cbcc10eac3f2e0aa8c2f2
Terrance <terrancehah@gmail.com>
2025-05-21 01:36:52 +0800

    chore: remove unused utility files and update footer margin

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const footer = document.createElement('footer');
     footer.className = 'footer'; // Add your footer's class name here
     footer.innerHTML = `
-    <div class="text-center mt-2 pb-2 mx-4">
+    <div class="text-center mt-2 pb-2">
         <h3 class="copyright sm:text-lg md:text-xl lg:text-xl font-lato font-semibold text-primary">Copyright © ${new Date().getFullYear()}, Terrance Hah.<br>
         </h3>
         <h4 class="cookie-notice sm:text-lg md:text-xl lg:text-lg font-raleway font-medium text-primary">This site does not store cookies on your computer.</h4>
